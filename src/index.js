45ebe4b7023e555a946278d23eb8749e814009b0
kathir <128504080+kathirks2003@users.noreply.github.com>
2023-09-21 09:38:06 +0530

    Created reusable components

--- src/index.js
+++ src/index.js
@@ -1,207 +1,210 @@
 // eslint-disable-next-line
 import vision from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3";
+import main from "./createFaceLandmarker";
 
 import * as tf from '@tensorflow/tfjs';
 // const fs = require("fs");
 // const { PDFDocument, rgb } = require("pdf-lib");
 
-// import {PDFDocument, rgb} from "pdf-lib";
-const { FaceLandmarker, FilesetResolver } = vision;
-const demosSection = document.getElementById("demos");
-const videoBlendShapes = document.getElementById("video-blend-shapes");
+main();
 
-let faceLandmarker;
-let runningMode = "IMAGE";
-let enableWebcamButton;
-let webcamRunning = false;
-const videoWidth = 480;
+// // import {PDFDocument, rgb} from "pdf-lib";
+// const { FaceLandmarker, FilesetResolver } = vision;
+// const demosSection = document.getElementById("demos");
+// const videoBlendShapes = document.getElementById("video-blend-shapes");
 
-const classFrequencies = [0, 0, 0, 0, 0, 0, 0, 0];
+// let faceLandmarker;
+// let runningMode = "IMAGE";
+// let enableWebcamButton;
+// let webcamRunning = false;
+// const videoWidth = 480;
 
-// Add a global variable to store the blendshapes data
-let blendShapesData = [];
+// const classFrequencies = [0, 0, 0, 0, 0, 0, 0, 0];
 
-const model = await tf.loadLayersModel('https://model-facelandmark.s3.us-west-2.amazonaws.com/model.json');
-console.log("Success");
+// // Add a global variable to store the blendshapes data
+// let blendShapesData = [];
 
-async function createFaceLandmarker() {
-  const filesetResolver = await FilesetResolver.forVisionTasks(
-    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
-  );
-  faceLandmarker = await FaceLandmarker.createFromOptions(filesetResolver, {
-    baseOptions: {
-      modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
-      delegate: "GPU"
-    },
-    outputFaceBlendshapes: true,
-    runningMode,
-    numFaces: 1
-  });
-  demosSection.classList.remove("invisible");
-}
-createFaceLandmarker();
+// const model = await tf.loadLayersModel('https://model-facelandmark.s3.us-west-2.amazonaws.com/model.json');
+// console.log("Success");
 
-const video = document.getElementById("webcam");
+// async function createFaceLandmarker() {
+//   const filesetResolver = await FilesetResolver.forVisionTasks(
+//     "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
+//   );
+//   faceLandmarker = await FaceLandmarker.createFromOptions(filesetResolver, {
+//     baseOptions: {
+//       modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+//       delegate: "GPU"
+//     },
+//     outputFaceBlendshapes: true,
+//     runningMode,
+//     numFaces: 1
+//   });
+//   demosSection.classList.remove("invisible");
+// }
+// createFaceLandmarker();
 
-const canvasElement = document.getElementById("output_canvas");
+// const video = document.getElementById("webcam");
 
-function hasGetUserMedia() {
-  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
-}
+// const canvasElement = document.getElementById("output_canvas");
 
-if (hasGetUserMedia()) {
-  enableWebcamButton = document.getElementById("webcamButton");
-  enableWebcamButton.addEventListener("click", enableCam);
-} else {
-  console.warn("getUserMedia() is not supported by your browser");
-}
+// function hasGetUserMedia() {
+//   return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+// }
 
-// to save the classFrequencies as json file
-// Function to save class frequencies as a JSON file
-function saveClassFrequenciesAsJSON() {
-  const classLabels = ["Angry", "Contempt", "Disgust", "Fear", "Happy", "Neutral", "Sad", "Surprise"];
-  const classFrequenciesWithLabels = {};
+// if (hasGetUserMedia()) {
+//   enableWebcamButton = document.getElementById("webcamButton");
+//   enableWebcamButton.addEventListener("click", enableCam);
+// } else {
+//   console.warn("getUserMedia() is not supported by your browser");
+// }
 
-  for (let i = 0; i < classLabels.length; i++) {
-    const label = classLabels[i];
-    classFrequenciesWithLabels[label] = classFrequencies[i];
-  }
+// // to save the classFrequencies as json file
+// // Function to save class frequencies as a JSON file
+// function saveClassFrequenciesAsJSON() {
+//   const classLabels = ["Angry", "Contempt", "Disgust", "Fear", "Happy", "Neutral", "Sad", "Surprise"];
+//   const classFrequenciesWithLabels = {};
 
-  const jsonData = JSON.stringify(classFrequenciesWithLabels, null, 2);
-  const blob = new Blob([jsonData], { type: "application/json" });
-  const url = URL.createObjectURL(blob);
+//   for (let i = 0; i < classLabels.length; i++) {
+//     const label = classLabels[i];
+//     classFrequenciesWithLabels[label] = classFrequencies[i];
+//   }
 
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = "class_frequencies.json";
-  a.click();
-}
+//   const jsonData = JSON.stringify(classFrequenciesWithLabels, null, 2);
+//   const blob = new Blob([jsonData], { type: "application/json" });
+//   const url = URL.createObjectURL(blob);
 
-function enableCam(event) {
-  if (!faceLandmarker) {
-    console.log("Wait! faceLandmarker not loaded yet.");
-    return;
-  }
+//   const a = document.createElement("a");
+//   a.href = url;
+//   a.download = "class_frequencies.json";
+//   a.click();
+// }
 
-  if (webcamRunning === true) {
-    webcamRunning = false;
+// function enableCam(event) {
+//   if (!faceLandmarker) {
+//     console.log("Wait! faceLandmarker not loaded yet.");
+//     return;
+//   }
 
-    videoBlendShapes.innerHTML = "";
-    if (video.srcObject) {
-      const tracks = video.srcObject.getTracks();
-      tracks.forEach((track) => {
-        track.stop();
-      });
-    }
-    video.srcObject = null;
-    enableWebcamButton.innerText = "ENABLE PREDICTIONS";
+//   if (webcamRunning === true) {
+//     webcamRunning = false;
 
-    // Save the blendShapesData as a JSON file
-    // Save the blendShapesData as a JSON file
-if (blendShapesData.length > 0) {
-  console.log(classFrequencies);
-  const jsonData = JSON.stringify(blendShapesData, null, 2);
-  const blob = new Blob([jsonData], { type: "application/json" });
-  const url = URL.createObjectURL(blob);
+//     videoBlendShapes.innerHTML = "";
+//     if (video.srcObject) {
+//       const tracks = video.srcObject.getTracks();
+//       tracks.forEach((track) => {
+//         track.stop();
+//       });
+//     }
+//     video.srcObject = null;
+//     enableWebcamButton.innerText = "ENABLE PREDICTIONS";
 
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = "blendshapes_data.json";
-  a.click();
+//     // Save the blendShapesData as a JSON file
+//     // Save the blendShapesData as a JSON file
+// if (blendShapesData.length > 0) {
+//   console.log(classFrequencies);
+//   const jsonData = JSON.stringify(blendShapesData, null, 2);
+//   const blob = new Blob([jsonData], { type: "application/json" });
+//   const url = URL.createObjectURL(blob);
 
-  saveClassFrequenciesAsJSON();
+//   const a = document.createElement("a");
+//   a.href = url;
+//   a.download = "blendshapes_data.json";
+//   a.click();
 
-}
-  } else {
-    webcamRunning = true;
-    enableWebcamButton.innerText = "DISABLE PREDICTIONS";
-    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
-      video.srcObject = stream;
-      video.addEventListener("loadeddata", predictWebcam);
-    });
+//   saveClassFrequenciesAsJSON();
 
-    if (blendShapesData.length === 0) {
-      blendShapesData = []; // Initialize the blendshapes data array
-    }
-  }
-}
+// }
+//   } else {
+//     webcamRunning = true;
+//     enableWebcamButton.innerText = "DISABLE PREDICTIONS";
+//     navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+//       video.srcObject = stream;
+//       video.addEventListener("loadeddata", predictWebcam);
+//     });
 
-async function predictWebcam() {
-  const radio = video.videoHeight / video.videoWidth;
-  video.style.width = videoWidth + "px";
-  video.style.height = videoWidth * radio + "px";
-  canvasElement.style.width = videoWidth + "px";
-  canvasElement.style.height = videoWidth * radio + "px";
-  canvasElement.width = video.videoWidth;
-  canvasElement.height = video.videoHeight;
+//     if (blendShapesData.length === 0) {
+//       blendShapesData = []; // Initialize the blendshapes data array
+//     }
+//   }
+// }
 
-  if (runningMode === "IMAGE") {
-    runningMode = "VIDEO";
-    await faceLandmarker.setOptions({ runningMode: runningMode });
-  }
+// async function predictWebcam() {
+//   const radio = video.videoHeight / video.videoWidth;
+//   video.style.width = videoWidth + "px";
+//   video.style.height = videoWidth * radio + "px";
+//   canvasElement.style.width = videoWidth + "px";
+//   canvasElement.style.height = videoWidth * radio + "px";
+//   canvasElement.width = video.videoWidth;
+//   canvasElement.height = video.videoHeight;
 
-  let startTimeMs = performance.now();
-  let lastVideoTime = -1;
-  let results = undefined;
+//   if (runningMode === "IMAGE") {
+//     runningMode = "VIDEO";
+//     await faceLandmarker.setOptions({ runningMode: runningMode });
+//   }
 
-  if (lastVideoTime !== video.currentTime) {
-    lastVideoTime = video.currentTime;
-    results = faceLandmarker.detectForVideo(video, startTimeMs);
-  }
-  // to push the blendshapes data into the array
-  if (results.faceBlendshapes) {
-    // console.log(results.faceBlendshapes[0]);
+//   let startTimeMs = performance.now();
+//   let lastVideoTime = -1;
+//   let results = undefined;
+
+//   if (lastVideoTime !== video.currentTime) {
+//     lastVideoTime = video.currentTime;
+//     results = faceLandmarker.detectForVideo(video, startTimeMs);
+//   }
+//   // to push the blendshapes data into the array
+//   if (results.faceBlendshapes) {
+//     // console.log(results.faceBlendshapes[0]);
     
-    let scores = [];
-    const inputJSON = results.faceBlendshapes[0];
-    // console.log(inputJSON);
-    if(inputJSON){
-    const categoryScores = inputJSON.categories.map(category => category.score);
-    // console.log(categoryScores);
-    scores = categoryScores;
-  }
-    if(scores.length!==0){
-      const input = tf.tensor(scores, [1,52]);
-      const output = model.predict(input);
-      const softmax = tf.softmax(output);
-      const probabilities = softmax.arraySync()[0];
-      const maxProbabilityIndex = probabilities.indexOf(Math.max(...probabilities));
-      classFrequencies[maxProbabilityIndex]++;
-      console.log(maxProbabilityIndex);
-      // console.log(softmax);
-    }
-    blendShapesData.push(results.faceBlendshapes);
-  }
+//     let scores = [];
+//     const inputJSON = results.faceBlendshapes[0];
+//     // console.log(inputJSON);
+//     if(inputJSON){
+//     const categoryScores = inputJSON.categories.map(category => category.score);
+//     // console.log(categoryScores);
+//     scores = categoryScores;
+//   }
+//     if(scores.length!==0){
+//       const input = tf.tensor(scores, [1,52]);
+//       const output = model.predict(input);
+//       const softmax = tf.softmax(output);
+//       const probabilities = softmax.arraySync()[0];
+//       const maxProbabilityIndex = probabilities.indexOf(Math.max(...probabilities));
+//       classFrequencies[maxProbabilityIndex]++;
+//       console.log(maxProbabilityIndex);
+//       // console.log(softmax);
+//     }
+//     blendShapesData.push(results.faceBlendshapes);
+//   }
 
-  drawBlendShapes(videoBlendShapes, results.faceBlendshapes);
+//   drawBlendShapes(videoBlendShapes, results.faceBlendshapes);
 
-  if (webcamRunning === true) {
-    window.requestAnimationFrame(predictWebcam);
-  }
-}
+//   if (webcamRunning === true) {
+//     window.requestAnimationFrame(predictWebcam);
+//   }
+// }
 
 
-function drawBlendShapes(el, blendShapes) {
-  if (!blendShapes || !blendShapes.length) {
-    return;
-  }
+// function drawBlendShapes(el, blendShapes) {
+//   if (!blendShapes || !blendShapes.length) {
+//     return;
+//   }
 
-  // explain how the below code works by using the comments below
+//   // explain how the below code works by using the comments below
 
-  let htmlMaker = "";
-  // eslint-disable-next-line
-  blendShapes[0].categories.map((shape) => {
-    htmlMaker += `
-      <li class="blend-shapes-item">
-        <span class="blend-shapes-label">${
-          shape.displayName || shape.categoryName
-        }</span>
-        <span class="blend-shapes-value" style="width: calc(${
-          +shape.score * 100
-        }% - 120px)">${(+shape.score).toFixed(2)}</span>
-      </li>
-    `;
-  });
+//   let htmlMaker = "";
+//   // eslint-disable-next-line
+//   blendShapes[0].categories.map((shape) => {
+//     htmlMaker += `
+//       <li class="blend-shapes-item">
+//         <span class="blend-shapes-label">${
+//           shape.displayName || shape.categoryName
+//         }</span>
+//         <span class="blend-shapes-value" style="width: calc(${
+//           +shape.score * 100
+//         }% - 120px)">${(+shape.score).toFixed(2)}</span>
+//       </li>
+//     `;
+//   });
 
-  el.innerHTML = htmlMaker;
-}
+//   el.innerHTML = htmlMaker;
+// }
